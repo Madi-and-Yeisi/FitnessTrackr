6534fbdcff5d9ed40b5b5f15a2f809ef482132b9
Madi-Webb <madilynwebb98@gmail.com>
2022-11-09 21:46:26 -0700

    set up express and pg client infastructure

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 //building express server
+
+const express = require('express');
+const { client } = require('./db/index');
+
+const app = express();
+
+client.connect();
+
+app.listen(3000, () => {
+    console.log('We are now running on port 3000');
+});
