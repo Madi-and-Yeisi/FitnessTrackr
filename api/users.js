d8c46a30daade34351c7d2e8c2cc842ad751a5d1
Madi-Webb <madilynwebb98@gmail.com>
2022-11-12 14:26:22 -0700

    built out file structure and added starter code

---
+++ api/users.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-useless-catch */
+const express = require("express");
+const router = express.Router();
+
+// POST /api/users/register
+console.log("hello world!");
+
+// POST /api/users/login
+
+// GET /api/users/me
+
+// GET /api/users/:username/routines
+
+module.exports = router;
--- db/index.js
+++ db/index.js
@@ -7,3 +7,12 @@ const client = new pg.Client(`postgres://localhost:5432/fitness-dev`);
 module.exports = {
     client
 }
+
+// from the forked example repo
+// module.exports = {
+//     // ...require('./client'), // adds key/values from users.js
+//     ...require('./users'), // adds key/values from users.js
+//     ...require('./activities'), // adds key/values from activites.js
+//     ...require('./routines'), // etc
+//     ...require('./routine_activities') // etc
+//   }
