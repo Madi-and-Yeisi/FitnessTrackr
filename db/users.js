5050d146c5ba4386c5b279152508b647c7b274b4
yeisih <113942349+yeisih@users.noreply.github.com>
2022-11-14 16:19:38 -0600

    Create user

--- db/users.js
+++ db/users.js
@@ -4,8 +4,22 @@ const client = require("./client");
 
 // user functions
 async function createUser({ username, password }) {
-  
+  try {
+    const {rows:[user]}= await client.query(`
+    INSERT INTO users (username,password)
+    VALUES ($1,$2)
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR (255)
+    RETURNING*;
+    `,[username,password]);
+    return user;
+  }catch (error){
+    throw error;
+  }
 }
+
+
 
 async function getUser({ username, password }) {
 
